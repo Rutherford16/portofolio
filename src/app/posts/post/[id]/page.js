2f34cba237d79f923d29dd0634a6d4573e4fc387
Rutherford16 <hidayatronny@ymail.com>
2023-09-12 22:08:23 +0800

    akhirnya berhasil generate static file

--- src/app/posts/post/[id]/page.js
+++ src/app/posts/post/[id]/page.js
@@ -1,33 +1,15 @@
-'use client'
-
 import Link from "next/link";
 import { marked } from "marked";
-import DOMPurify from "dompurify";
 import posts from "@/app/json/posts.json";
 
-// export async function generateStaticParams() {
-//     return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }]
-// }
-
-// Post.getInitialProps = async (ctx) => {
-//     return posts.posts.map((p) => ({
-//         id: p.id.toString(),
-//     }))
-// }
-
 export async function generateStaticParams() {
-    return posts.map((p, i) => ({
+    return posts.map((p) => ({
         id: p.id.toString(),
     }))
 }
 
 export default function Post({ params: { id } }) {
     const post = posts.find(p => p.id.toString() === id)
-    // const post = posts[id] || {};
-    posts.map((p, i) => (
-        console.log(i)
-    ))
-    console.log(post)
 
     return (
         <>
@@ -39,7 +21,7 @@ export default function Post({ params: { id } }) {
                     <h1 className="text-2xl text-center font-bold">{post.judul}</h1>
                     <h5 className="text-xs text-center">Dibuat pada : {post.tanggal}</h5>
                     <hr />
-                    <article className="indent-8 text-justify w-full max-w-none prose mt-4 prose-blockquote:text-primary prose-blockquote:bg-secondary prose-blockquote:p-1 prose-strong:text-primary prose-headings:text-primary prose-code:text-red-400 prose-a:no-underline prose-a:text-blue-700 hover:prose-a:opacity-80" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked.parse(post.isi)) }} />
+                    <article className="indent-8 text-justify w-full max-w-none prose mt-4 prose-blockquote:text-primary prose-blockquote:bg-secondary prose-blockquote:p-1 prose-strong:text-primary prose-headings:text-primary prose-code:text-red-400 prose-a:no-underline prose-a:text-blue-700 hover:prose-a:opacity-80" dangerouslySetInnerHTML={{ __html: marked.parse(post.isi) }} />
                 </div>
             ) : (
                 <div className="w-full p-5">
