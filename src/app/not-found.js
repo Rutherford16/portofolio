3d236e0cbed146e2b022b3623ee139aac706a1e9
Rutherford16 <hidayatronny@ymail.com>
2023-09-19 16:53:34 +0800

    update

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,13 +1,11 @@
 import Link from 'next/link'
- 
+
 export default async function NotFound() {
   return (
-    <div>
-      <h2>Page Not Found</h2>
+    <div className='w-full flex justify-center'>
+      <h2 className='text-xl'>Page Not Found</h2>
       <p>Could not find requested resource</p>
-      <p>
-        View <Link href="/">Beranda</Link>
-      </p>
+      <Link href="/" className='p-2 border rounded'>Beranda</Link>
     </div>
   )
 }
