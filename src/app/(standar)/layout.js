9d416c157a0a2e73092a32009527d3a8aa6a460c
Rutherford16 <hidayatronny@ymail.com>
2023-09-21 15:18:42 +0800

    update

--- src/app/(standar)/layout.js
+++ src/app/(standar)/layout.js
@@ -7,14 +7,14 @@ import NavLink from './components/navlink';
 import Blackadders from './animasi/material/blackadders';
 import Badge from './components/badge';
 
-export const metadata = {
-  title: 'Rutherford16',
-  description: 'Website pribadi milik Ronny Hidayat',
-  // meta: {
-  //   name: 'google-site-verification',
-  //   content: 'jzlNvMROC1_8KOdnf2z-b7V7r1CGKz9g8mUmHHviI2E',
-  // },
-}
+// export const metadata = {
+//   title: 'Rutherford16',
+//   description: 'Website pribadi milik Ronny Hidayat',
+//   // meta: {
+//   //   name: 'google-site-verification',
+//   //   content: 'jzlNvMROC1_8KOdnf2z-b7V7r1CGKz9g8mUmHHviI2E',
+//   // },
+// }
 
 export default function RootLayout({ children }) {
   const [loading, setLoading] = useState(true);
@@ -90,6 +90,10 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <meta name='google-site-verification' content='jzlNvMROC1_8KOdnf2z-b7V7r1CGKz9g8mUmHHviI2E' />
+        <title>Rutherford16</title>
+      </head>
       <body>
         {loading ? (
           <main className="w-screen h-screen bg-background text-primary font-firaMono border-primary">
